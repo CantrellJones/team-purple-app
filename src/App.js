1446a0445a14f3dd320a4ea90ea65dc399abf741
CantrellJones <cantrelljones@gmail.com>
2025-06-08 15:10:24 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './LandingPage.css';
+import './LandingPageFixed.css';
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
 
